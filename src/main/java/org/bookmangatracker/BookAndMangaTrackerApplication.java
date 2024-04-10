3c129bbaebbc26d9cd67bda160b2d945412ef639
Marco Josefsen <majos6959@oslomet.no>
2024-04-10 18:46:00 +0200

    Initial commit

---
+++ src/main/java/org/bookmangatracker/BookAndMangaTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.bookmangatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAndMangaTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookAndMangaTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bookmangatracker/BookAndMangaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bookmangatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAndMangaTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
