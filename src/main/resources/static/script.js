51c776cc91148e085764d180ec94209bbb8f08e2
Marco Josefsen <majos6959@oslomet.no>
2024-04-11 11:36:30 +0200

    Added basic formating of data, and basic searching features

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,21 +1,39 @@
-const title = "Solo Leveling"; //Skal være get element by id
-const baseURL = "https://api.mangadex.org";
+function search() {
+    const title = $("#searchInput").val();
+    const baseURL = "https://api.mangadex.org";
 
-const searchURL = `${baseURL}/manga?title=${encodeURIComponent(title)}`;
+    const searchURL = `${baseURL}/manga?title=${encodeURIComponent(title)}`;
 
 
-$.get(searchURL, function(response) {
-    console.log(response);
+    $.get(searchURL, function (response) {
+        //Response is what comes up when searching for "title", multiple different additions can come up
+        console.log(response);
+        console.log(title);
 
-    if(!response.result) {
-        console.log("Response is not OK");
-    }
-    else {
-        console.log("Response is OK");
-        const mangaData = response.data;
-        mangaData.forEach(manga => {
-            console.log(manga);
-        });
-    }
+        if (response.total === 0) {
+            console.log("Search is empty");
+        } else {
+            console.log("Response is OK");
+            const mangaData = response.data;
+            //Makes it so previous searches are removed
+            $("#results").html("");
+            //Lists the different mangas separately when searching for "title"
+            mangaData.forEach(manga => {
+                console.log(manga);
+                // Goes into the manga, attributes, descripton and selects the English version from the object
+                console.log(manga.attributes.description.en);
+                console.log(manga.attributes.title.en);
+                //$("#results").append(manga.attributes.description.en + "<br><br>");
+                formatData(manga)
+            });
+        }
+    });
+}
 
-});
+function formatData(manga) {
+    //img først egentlig
+    let print = "<h2>" + manga.attributes.title.en +  "</h2>";
+    print += "<button>Add to read</button>";
+    print += "<p>" + manga.attributes.description.en + "</p>";
+    $("#results").append(print);
+}
