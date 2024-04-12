7a0d061a71452c60b0cbfdee8f9d426269ca9c74
Marco Josefsen <majos6959@oslomet.no>
2024-04-12 19:45:52 +0200

    Fixed issues with image not coming up

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -2,38 +2,58 @@ function search() {
     const title = $("#searchInput").val();
     const baseURL = "https://api.mangadex.org";
 
-    const searchURL = `${baseURL}/manga?title=${encodeURIComponent(title)}`;
-
+    const searchURL = `${baseURL}/manga?title=${encodeURIComponent(title)}&includes[]=cover_art`;
 
     $.get(searchURL, function (response) {
         //Response is what comes up when searching for "title", multiple different additions can come up
         console.log(response);
-        console.log(title);
 
         if (response.total === 0) {
             console.log("Search is empty");
         } else {
             console.log("Response is OK");
             const mangaData = response.data;
+
             //Makes it so previous searches are removed
             $("#results").html("");
             //Lists the different mangas separately when searching for "title"
             mangaData.forEach(manga => {
                 console.log(manga);
-                // Goes into the manga, attributes, descripton and selects the English version from the object
-                console.log(manga.attributes.description.en);
-                console.log(manga.attributes.title.en);
-                //$("#results").append(manga.attributes.description.en + "<br><br>");
-                formatData(manga)
+
+                //Gets the url for the image, this method also calls formatData() which formats data and puts in on screen
+                getImage(manga);
             });
         }
     });
 }
 
-function formatData(manga) {
-    //img først egentlig
-    let print = "<h2>" + manga.attributes.title.en +  "</h2>";
+function getImage(manga) {
+    const coverRelationships = manga.relationships.find((relationship) => relationship.type = "cover_art" && relationship.attributes && relationship.attributes.fileName);
+    const coverFilename = coverRelationships.attributes.fileName;
+    const coverID = coverRelationships.id;
+    const mangaId = manga.id;
+
+    $.get(`https://api.mangadex.org/cover/${coverID}`, function(coverResponse) {
+        if(coverResponse.result === "ok") {
+            console.log("Cover filename: " + coverFilename);
+
+            const coverURL = `https://uploads.mangadex.org/covers/${mangaId}/${coverFilename}`;
+            console.log("Cover URL: " + coverURL);
+
+            formatData(manga, coverURL);
+        }
+        else {
+            console.error("Error fetching cover details:" + coverResponse.errors);
+        }
+    });
+}
+
+function formatData(manga, coverURL) {
+    let print = "<div class='manga-entry'>";
+    print += "<div class='image-container'><img src='" + coverURL + "'></div>";
+    print += "<h2>" + manga.attributes.title.en +  "</h2>";
     print += "<button>Add to read</button>";
     print += "<p>" + manga.attributes.description.en + "</p>";
+    print += "</div>";
     $("#results").append(print);
 }
