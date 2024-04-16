031d12700e276346c9870ef4d31fa77121d1614f
Marco Josefsen <majos6959@oslomet.no>
2024-04-16 09:27:19 +0200

    Added rating

--- src/main/java/org/bookmangatracker/Read.java
+++ src/main/java/org/bookmangatracker/Read.java
@@ -3,12 +3,14 @@ package org.bookmangatracker;
 public class Read {
     private int userid;
     private int mangaid;
+    private double rating;
 
     public Read(){}
 
-    public Read(int userid, int mangaid) {
+    public Read(int userid, int mangaid, double rating) {
         this.userid = userid;
         this.mangaid = mangaid;
+        this.rating = rating;
     }
 
     public int getUserid() {
@@ -25,5 +27,13 @@ public class Read {
 
     public void setMangaid(int mangaid) {
         this.mangaid = mangaid;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
     }
 }
