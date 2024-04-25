202803c5c4fc920207afcecd24788093478db1cc
Marco Josefsen <majos6959@oslomet.no>
2024-04-25 18:15:37 +0200

    Added so slideshows get data from api (still not finished), and fills in searches upon loading the webpage. Formating of code still needs to be done

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -3,14 +3,14 @@ let inputCounter = 0;
 /* Fills the preview on index.html with mangas */
 $( document ).ready(function() {
     userInHeader();
-    showSlideshow();
+    getMangaForSlideshow();
+    previewList();
     listRead();
-
-    //search(); KAN KANKSJE LAGE EN DUPLICATE SEARCH UNDER SOM DA IKKE GJØR PREVIEW EMPTY SÅ JEG KAN HA SLIDESHOW SÅ SEARCHEN UNDER
 });
 
 function search() {
     $("#preview").empty();
+    $("#manySearch").empty();
 
     const title = $("#searchInput").val();
 
@@ -236,8 +236,19 @@ async function userInHeader() {
     $(".user-info").html(ut);
 }
 
-function showSlideshow() {
-    let print = "<div id='carouselIndicators' class='carousel slide' data-bs-ride='carousel'>";
+function showSlideshow(manga1, coverURL1) {
+    /*
+    const inManga = {
+        title : manga.attributes.title.en,
+        imageURL : coverURL
+    }
+       print += "<div class='image-container'><img src='" + coverURL + "'></div>";
+     */
+
+    let print = "<div class='container'>";
+    print += "<div class='row justify-content-center'>";
+    print += "<div class='col-md-6'>";
+    print += "<div id='carouselIndicators' class='carousel slide' data-bs-ride='carousel'>";
     print += "<ol class='carousel-indicators'>";
     print += "<li data-bs-target='#carouselIndicators' data-bs-slide-to='0' class='active'></li>";
     print += "<li data-bs-target='#carouselIndicators' data-bs-slide-to='1'></li>";
@@ -245,20 +256,86 @@ function showSlideshow() {
     print += "<div class='carousel-inner'>";
     //TODO: Data-bs-interval funker ikke, det er BS
     print += "<div class='carousel-item active' data-bs-interval='1000'>";
-    print += "<img class='d-block w-100' src='manga1.jpg' alt='test'>";
+    print += "<img class='d-block w-100' src='" + coverURL1 + "' alt='test'>";
     print += "</div>";
     print += "<div class='carousel-item' data-bs-interval='1000'>";
     print += "<img class='d-block w-100' src='manga2.jpg' alt='test'>";
     print += "</div>";
     print += "</div>";
     print += "</div>";
+    print += "</div>";
+    print += "</div>";
+    print += "</div>";
 
     $("#preview").html(print);
+
 
     // Trigger the carousel to start sliding
     //$('.carousel').carousel();
 }
 
 function getMangaForSlideshow() {
+    const title1 = "Frieren";
+    const title2 = "Solo Leveling";
 
+    const baseURL = "https://api.mangadex.org";
+
+    const searchURL1 = `${baseURL}/manga?title=${encodeURIComponent(title1)}&includes[]=cover_art`;
+    const searchURL2 = `${baseURL}/manga?title=${encodeURIComponent(title2)}&includes[]=cover_art`;
+
+    $.get(searchURL1, function (response) {
+        const manga = response.data[0];
+
+        const coverRelationships = manga.relationships.find((relationship) => relationship.type = "cover_art" && relationship.attributes && relationship.attributes.fileName);
+        const coverFilename = coverRelationships.attributes.fileName;
+        const coverID = coverRelationships.id;
+        const mangaId = manga.id;
+
+        $.get(`https://api.mangadex.org/cover/${coverID}`, function(coverResponse) {
+            if(coverResponse.result === "ok") {
+                const coverURL = `https://uploads.mangadex.org/covers/${mangaId}/${coverFilename}`;
+
+                showSlideshow(manga, coverURL);
+            }
+            else {
+                console.error("Error fetching cover details:" + coverResponse.errors);
+            }
+        });
+    });
+}
+
+function previewList() {
+    const title = "";
+
+    const baseURL = "https://api.mangadex.org";
+
+    const searchURL = `${baseURL}/manga?title=${encodeURIComponent(title)}&includes[]=cover_art`;
+
+    $.get(searchURL, function (response) {
+        const mangaData = response.data;
+
+            //Lists the different mangas separately when searching for "title"
+            mangaData.forEach(manga => {
+                formatPreviewData(manga);
+            });
+    });
+}
+
+function formatPreviewData(manga) {
+    const coverRelationships = manga.relationships.find((relationship) => relationship.type = "cover_art" && relationship.attributes && relationship.attributes.fileName);
+    const coverFilename = coverRelationships.attributes.fileName;
+    const coverID = coverRelationships.id;
+    const mangaId = manga.id;
+
+    $.get(`https://api.mangadex.org/cover/${coverID}`, function(coverResponse) {
+        const coverURL = `https://uploads.mangadex.org/covers/${mangaId}/${coverFilename}`;
+
+        let print = "<div class='manga-entry'>";
+        print += "<div class='image-container'><img src='" + coverURL + "'></div>";
+        print += "<h2 class='manga-title'>" + manga.attributes.title.en +  "</h2>";
+        print += "<p class='manga-description'>" + manga.attributes.description.en + "</p>";
+        print += "</div>";
+
+        $("#manySearch").append(print);
+    });
 }
